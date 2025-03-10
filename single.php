e0bb636af9db37e36de7162a206a96a0b01d88a0
Scott Limmer <scottlimmer@gmail.com>
2025-03-10 14:04:15 +1100

    LSP-21 Finish layout for page single (#17)

--- single.php
+++ single.php
@@ -9,25 +9,22 @@
 // Single post
 get_header();
 ?>
+    <div class="sidebar">
 
-    <div class="row">
-        <?php //get_sidebar(); ?>
-        <div class="col-xs-12 col-md-9 main">
-            <?php //get_breadcrumb(); ?>
-
-            <div class="content">
-                <?php
-                if (have_posts()) :
-                    while (have_posts()) : the_post(); ?>
-                        <h1><?php the_title() ?></h1>
-                        <?php
-                        the_content();
-                    endwhile;
-                endif;
-                ?>
-            </div>
-        </div>
     </div>
+
+    <div class="content">
+		<?php
+		if ( have_posts() ) :
+			while ( have_posts() ) : the_post(); ?>
+                <h1><?php the_title() ?></h1>
+				<?php
+				the_content();
+			endwhile;
+		endif;
+		?>
+    </div>
+
 <?php
 
 get_footer();
