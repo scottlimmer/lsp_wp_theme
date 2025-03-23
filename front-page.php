9ff2552843fea5dff06e5f3a46b2dfc2e38ebdd8
Scott Limmer <scottlimmer@gmail.com>
2025-03-23 07:53:32 +0000

    LSP-32 Improve home page layout LSP-25 LSP-26 LSP-31

--- front-page.php
+++ front-page.php
@@ -13,8 +13,9 @@ get_header();
 
 ?>
 
-    <div class="intro">
-        <div class="intro-text">
+    <article>
+
+        <div class="text-tile intro-text">
 			<?php
 			if ( have_posts() ) :
 				while ( have_posts() ) : the_post(); ?>
@@ -24,75 +25,101 @@ get_header();
 			endif;
 			?>
         </div>
-        <div class="intro-side">
-			<?php
-			$page = get_page_by_slug( 'report-a-sighting' );
-			if ( $page ): ?>
 
-
-				<?php if ( has_post_thumbnail( $page ) ) :
-					$image_url = wp_get_attachment_image_url( get_post_thumbnail_id( $page ), 'tile-thumbnail' );
-				endif; ?>
-
-                <a
-                        class="tile"
-                        href="<?php echo get_permalink( $page ); ?>"
-                        style="<?= $image_url ? "background-image: url({$image_url}); " : '' ?>"
-                >
-
-                    <span><?= $page->post_title ?></span>
-                </a>
-			<?php
-			endif;
+        <div class="text-tile survey-dates">
+            <i class="fa-solid fa-calendar-days fa-2x"></i> <?php
+			echo get_field( 'survey_dates' ) ?: 'No survey dates have been scheduled for this season.';
 			?>
+        </div>
+
+
+        <div class="text-tile news">
+            <h2>News</h2>
+            <ul>
+				<?php
+
+				$args = [
+					'posts_per_page' => 3,
+					'post_type'      => 'post',
+					'orderby'        => 'date',
+					'order'          => 'DESC',
+				];
+
+				$query = new WP_Query( $args );
+				if ( $query->have_posts() ) {
+					while ( $query->have_posts() ) :
+						$query->the_post();
+
+						printf(
+							'<li><a href="%s">%s</a></li>',
+							get_the_permalink(),
+							get_the_title()
+						);
+
+					endwhile;
+				}
+				wp_reset_postdata();
+				?>
+
+            </ul>
+            <a class="more-news" href="<?php print_r( get_permalink( get_news_page()->ID ) ) ?>">More news
+                &rarr;</i></a>
+
+        </div>
+    </article>
+
+    <aside>
+		<?php
+		$page = get_page_by_slug( 'report-a-sighting' );
+		if ( $page ): ?>
+
+			<?php if ( has_post_thumbnail( $page ) ) :
+				$image_url = wp_get_attachment_image_url( get_post_thumbnail_id( $page ), 'tile-thumbnail' );
+			endif; ?>
 
             <a
-                    class="tile support"
-                    href="https://www.supportlathamssnipe.au/"
-                    style="background-image: url(<?= get_template_directory_uri() . "/assets/images/Lathams+Snipe-adobe-stock.jpg" ?>">
-                <span>Support the project</span>
+                    class="tile"
+                    href="<?php echo get_permalink( $page ); ?>"
+                    style="<?= $image_url ? "background-image: url({$image_url}); " : '' ?>"
+            >
+                <span><?= $page->post_title ?></span>
             </a>
-
-            <a class=" tile facebook" href="https://www.facebook.com/groups/345220814765069/">
-                <i class="fa-brands fa-facebook fa-3x"></i> Latham’s Snipe Project on Facebook
-            </a>
-        </div>
-    </div>
-
-    <div class="news">
-        <h2>News</h2>
 		<?php
-
-		$args = [
-			'posts_per_page' => 1,
-			'post_type'      => 'post',
-			'orderby'        => 'date',
-			'order'          => 'DESC',
-		];
-
-		$query = new WP_Query( $args );
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) :
-				$query->the_post();
-
-				get_template_part( 'partial/news-item-compact' );
-
-			endwhile;
-		}
-		wp_reset_postdata();
+		endif;
 		?>
-        <a class="more-news" href="<?php print_r( get_permalink( get_news_page()->ID ) ) ?>">More news &rarr;</i></a>
-    </div>
-    <div class="gallery-tile">
-        <h2>Gallery</h2>
-        <figure>
-            <a href="/gallery">
-                <img alt="Latham's Snipe" title="Latham's Snipe"
-                     src="/wp-content/uploads/2025/01/MarkLeathlean-Lathams-SnipeBalnarring070119-37-Edit-Editweb.jpg">
-            </a>
-            <figcaption>&copy; Photo by Mark Lethlean</figcaption>
-        </figure>
-    </div>
 
+        <a
+                class="tile support"
+                href="https://www.supportlathamssnipe.au/"
+                style="background-image: url(<?= get_template_directory_uri() . "/assets/images/Lathams+Snipe-adobe-stock.jpg" ?>)"
+                target="_blank"
+        >
+            <span>Support the project</span>
+        </a>
+
+        <a
+                class="tile facebook"
+                href="https://www.facebook.com/groups/345220814765069/"
+                target="_blank"
+        >
+            <i class="fa-brands fa-facebook fa-3x"></i> Latham’s Snipe Project on Facebook
+        </a>
+
+
+        <a class="tile"
+           href="/gallery"
+           title="&copy; Photo by Mark Lethlean"
+           style="background-image: url(<?= get_template_directory_uri() . "/assets/images/MarkLeathlean-Lathams-snipe-thumb.jpg" ?>)"
+        >
+            <span>Gallery</span>
+        </a>
+
+        <a class="tile documentary"
+           href="https://youtu.be/uKouzoQmspY"
+           target="_blank">
+            <img src="<?= get_template_directory_uri() . "/assets/images/snipe_poster.jpg" ?>" alt="SNIPE: The Latham's Snipe Project - A Film Story">
+        </a>
+
+    </aside>
 <?php
 get_footer();
